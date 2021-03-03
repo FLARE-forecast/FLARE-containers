983cb2bcbc24035e54c38e60ac0d62e54bc755a7
vahid-dan <v.daneshmand@gmail.com>
2021-03-03 14:49:40 -0500

    Add Tag Support

--- commons/flare-install.sh
+++ commons/flare-install.sh
@@ -18,9 +18,9 @@ DIRECTORY_HOST="/opt/flare"
 DIRECTORY_HOST_SHARED="/opt/flare/shared"
 
 CONTAINER_NAME=${1}
-CONFIG_URL=https://raw.githubusercontent.com/FLARE-forecast/FLARE-containers/master/${CONTAINER_NAME}/${CONFIG}
-HOST_SCRIPT_URL=https://raw.githubusercontent.com/FLARE-forecast/FLARE-containers/master/commons/${HOST_SCRIPT}
-COMMONS_SCRIPT_URL=https://raw.githubusercontent.com/FLARE-forecast/FLARE-containers/master/commons/${COMMONS_SCRIPT}
+CONFIG_URL=https://raw.githubusercontent.com/FLARE-forecast/FLARE-containers/${CONTAINER_VERSION}/${CONTAINER_NAME}/${CONFIG}
+HOST_SCRIPT_URL=https://raw.githubusercontent.com/FLARE-forecast/FLARE-containers/${CONTAINER_VERSION}/commons/${HOST_SCRIPT}
+COMMONS_SCRIPT_URL=https://raw.githubusercontent.com/FLARE-forecast/FLARE-containers/${CONTAINER_VERSION}/commons/${COMMONS_SCRIPT}
 
 # Bypass sudo Command for root
 sudo ()
