3176d6c8bbc67209c49754d09a4c6123b10db936
Vahid Daneshmand <v.daneshmand@gmail.com>
2022-03-08 14:24:57 -0500

    version 22.1.1

--- flare/release.sh
+++ flare/release.sh
@@ -20,7 +20,8 @@ git add -A
 git commit -m "version $version"
 git tag -a "$version" -m "version $version"
 git push
-git push --tagsdocker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+git push --tags
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 
 # Push it
 docker push $USERNAME/$IMAGE:latest
